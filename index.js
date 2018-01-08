c9507c97a8bcfd0ceab486d8abd9bed3002b4760
Robert <dominicusrobert@yahoo.com>
2018-01-08 18:55:11 +0700

    fixing belas

--- index.js
+++ index.js
@@ -18,21 +18,46 @@ function in_words(angka){
         return ANGKA_UTAMA[angka]; 
     }
     else if(totalAngka == 2){
+        var puluhan = '';
+        var satuan = '';
+
         if(arrStringAngka[0] == '1'){
-            return ANGKA_UTAMA_GABUNGAN[angka] + BELAS;
+            puluhan = ANGKA_UTAMA_GABUNGAN[1] + BELAS;
         }
         else{
-            return ANGKA_UTAMA[arrStringAngka[0]] + PULUH + ANGKA_UTAMA[arrStringAngka[1]];
+            puluhan = ANGKA_UTAMA[arrStringAngka[0]] + PULUH;
+            satuan = ANGKA_UTAMA[arrStringAngka[1]];
         }
+
+        return puluhan + satuan;
     }
     else if(totalAngka == 3){
-        return ANGKA_UTAMA_GABUNGAN[arrStringAngka[0]] + RATUS + ANGKA_UTAMA[arrStringAngka[1]] + PULUH + ANGKA_UTAMA[arrStringAngka[2]];
+        var ratusan = ANGKA_UTAMA_GABUNGAN[arrStringAngka[0]] + RATUS;
+         
+        var puluhan = '';
+        var satuan = '';
+
+        if(arrStringAngka[1] == '0'){
+            puluhan = '';
+        }
+        else if(arrStringAngka[1] == '1'){
+            puluhan = ANGKA_UTAMA_GABUNGAN[arrStringAngka[1]] + BELAS;
+        }
+        else{
+            puluhan = ANGKA_UTAMA[arrStringAngka[1]] + PULUH;
+            satuan = ANGKA_UTAMA[arrStringAngka[2]];
+        }
+
+
+        return  ratusan + puluhan + satuan;
     }
 
 }
 
 console.log(in_words(4)); // empat
+console.log(in_words(11)); // sebelas
 console.log(in_words(27)); // dua puluh tujuh
 console.log(in_words(102)); // seratus dua
+console.log(in_words(111)); // seratus sebelas
 // in_words(38079); // tiga uluh delapan ribu tujuh puluh sembilan
 // in_words(82102713); // delapan puluh dua juta seratus dua ribu tujuh ratus tiga belas
