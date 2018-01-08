ad4d8411dbbeef6af7f3690109fc7d8746a5e8f8
Robert <dominicusrobert@yahoo.com>
2018-01-08 18:05:36 +0700

    first init

---
+++ index.js
@@ -0,0 +1,10 @@
+function in_words(angka){
+
+}
+
+
+in_words(4); // empat
+in_words(27); // dua puluh tujuh
+in_words(102); // seratus dua
+in_words(38079); // tiga uluh delapan ribu tujuh puluh sembilan
+in_words(82102713); // delapan puluh dua juta seratus dua ribu tujuh ratus tiga belas
