31f55a85e8d85d9d1ee6353131b09c234a72ee06
Luthfi <vlootfie@gmail.com>
2018-01-08 16:21:50 +0700

    release 1

--- index.js
+++ index.js
@@ -15,9 +15,29 @@ const in_words = (num) => {
       return in_words((num - (num % 10)) / 10) + ' puluh';
     }
   } else if (num < 200) {
-    return 'seratus ' + in_words(num % 100);
+    if (num % 100 !== 0) {
+      return 'seratus ' + in_words(num % 100);
+    } else {
+      return 'seratus';
+    }
   } else if (num < 1000) {
-    return in_words((num - (num % 100)) / 100) + ' ratus ' + in_words(num % 100);
+    if (num % 100 !== 0) {
+      return in_words((num - (num % 100)) / 100) + ' ratus ' + in_words(num % 100);
+    } else {
+      return in_words((num - (num % 100)) / 100) + ' ratus';
+    }
+  } else if (num < 2000) {
+    if (num % 1000 !== 0) {
+      return in_words((num - (num % 1000)) / 1000) + ' ribu ' + in_words(num % 1000);
+    } else {
+      return in_words((num - (num % 1000)) / 1000) + ' ribu';
+    }
+  } else if (num < 1000000) {
+    if (num % 1000 !== 0) {
+      return in_words((num - (num % 1000)) / 1000) + ' ribu ' + in_words(num % 1000);
+    } else {
+      return in_words((num - (num % 1000)) / 1000) + ' ribu';
+    }
   }
 }
 
@@ -25,5 +45,5 @@ console.log(in_words(1));
 console.log(in_words(19));
 console.log(in_words(90));
 console.log(in_words(199));
-console.log(in_words(120));
+console.log(in_words(230));
 console.log(in_words(999));
