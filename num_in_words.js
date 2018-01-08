2edda2bc0f14a9bbbedd4d730003026c8460179c
aldiws <syldie.aldiwijaya@gmail.com>
2018-01-08 22:41:35 +0700

    release-1-recursive-done

--- num_in_words.js
+++ num_in_words.js
@@ -1,66 +1,76 @@
-let satuan = function satuan(num) {
-  switch (num) {
-    case '0':
-      return 'nol';
-    case '1':
-      return 'satu';
-    case '2':
-      return 'dua';
-    case '3':
-      return 'tiga';
-    case '4':
-      return 'empat';
-    case '5':
-      return 'lima';
-    case '6':
-      return 'enam';
-    case '7':
-      return 'tujuh';
-    case '8':
-      return 'delapan';
-    case '9':
-      return 'sembilan';
+function in_words(number) {
+  // Your code here
+  function belowHundred(num) {
+    let satuan = function satuan(num) {
+      switch (num) {
+        case 1:
+          return 'satu';
+          break;
+        case 2:
+          return 'dua';
+          break;
+        case 3:
+          return 'tiga';
+          break;
+        case 4:
+          return 'empat';
+          break;
+        case 5:
+          return 'lima';
+          break;
+        case 6:
+          return 'enam';
+          break;
+        case 7:
+          return 'tujuh';
+          break;
+        case 8:
+          return 'delapan';
+          break;
+        case 9:
+          return 'sembilan';
+          break;
+        default:
+          return '';
+      }
+    }
+
+    let belasan = function belasan(num) {
+      if (num === 10) return 'sepuluh'
+      else if (num === 11) return 'sebelas'
+      else if (num >= 11 && num <= 19) return `${satuan(num % 10)} belas`
+      return ''
+    }
+    let puluhan = function puluhan(num) {
+      return `${satuan(Math.floor(num / 10))} puluh ${satuan(num % 10)}`
+    }
+
+    if (num <= 9) return satuan(num)
+    else if (num >= 10 && num <= 19) return belasan(num)
+    else if (num >= 20 && num <= 99) return puluhan(num)
+    return ''
   }
-}
 
-let puluhan = function puluhan(num) {
-  let head = num[0];
-  let last = num[1];
-
-  if (num === '10') return 'sepuluh' //10
-  else if (num === '11') return 'sebelas' //11
-  else if (head === '1') return `${satuan(last)} belas` //12-19  
-  else if (head === '0') return satuan(last)
-  else if (last === '0') return `${satuan(head)} puluh` //20-90
-  return `${satuan(head)} puluh ${satuan(last)}` // 21-99
-}
-
-let ratusan = function ratusan(num) {
-  let head = num[0]
-  let middle = num[1]
-  let last = num[2]
-  let tail = middle + last
-
-  if (num === '100') return 'seratus'
-  else if (tail === '00') return `${satuan(head)} ratus`
-  else if (head === '0' || head === '1') return `seratus ${puluhan(tail)}`
-  return `${satuan(head)} ratus ${puluhan(tail)}`
-}
-
-
-function in_words(num) {
-  let splitNum = num.toString().split('')
-  if (splitNum.length == 1) {
-    console.log(satuan(String(num)))
-  } else if (splitNum.length == 2) {
-    console.log(puluhan(String(num)))
-  } else if (splitNum.length == 3) {
-    console.log(ratusan(String(num)))
+  if (number <= 99) return belowHundred(number)
+  else if (number >= 100 && number <= 199) {
+    return `seratus ${belowHundred(number % 100)}`
+  } else if (number >= 200 && number <= 999) {
+    return `${belowHundred(Math.floor(number / 100))} ratus ${belowHundred(number % 100)}`
+  } else if (number >= 1000 && number <= 1999) {
+    return `seribu ${in_words(number % 1000)}`
+  } else if (number >= 2000 && number <= 999999) {
+    return `${in_words(Math.floor(number / 1000))} ribu ${in_words(number % 1000)}`
+  } else if (number >= 1000000 && number <= 999999999) {
+    return `${in_words(Math.floor(number / 1000000))} juta ${in_words(number % 1000000)}`
+  } else if (number >= 1000000000 && number <= 999999999999) {
+    return `${in_words(Math.floor(number / 1000000000))} milyar ${in_words(number % 1000000000)}`
+  } else if (number >= 1000000000000 && number <= 999999999999999) {
+    return `${in_words(Math.floor(number / 1000000000000))} biliun ${in_words(number % 1000000000000)}`
+  } else if (number >= 1000000000000000 && number <= 999999999999999999) {
+    return `${in_words(Math.floor(number / 1000000000000000))} triliun ${in_words(number % 1000000000000000)}`
   }
+  return ''
 }
 
-
-in_words(4)
-in_words(27)
-in_words(102)
-in_words(999)
+// Driver code
+console.log(in_words(991888338));
