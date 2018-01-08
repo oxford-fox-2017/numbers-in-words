3008f6ddff59ce8359e5359a0bc7bdfd06540352
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-01-08 22:23:38 +0700

    baru sampai 999 dan recursive

--- number_to_word.js
+++ number_to_word.js
@@ -1,15 +1,18 @@
 function in_words(num){
-    let satuan = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan','sepuluh','sebelas'];
-    let puluhan = ['belas', 'puluh', ''];
-
-    let temp = '';
+    let satuan = ['', 'Satu', 'Dua', 'Tiga', 'Empat', 'Lima', 'Enam', 'Tujuh', 'Delapan', 'Sembilan','Sepuluh','Sebelas'];
 
     if ( num < 12){
         return satuan[num];
-    }else if ( num >=12 || num <=19){
-        return satuan[num.toString()[1]] + ' ' + puluhan[0];
+    }else if ( num >= 12 && num <=19){
+        return satuan[num%10] + ' Belas';
+    }else if ( num >= 20 && num <= 99){
+        return in_words(Math.floor(num/10)) +  ' Puluh ' + satuan[num%10];
+    }else if ( num >= 100 && num <= 199){
+        return 'Seratus ' + in_words(num %100);
+    }else if ( num >= 200 && num <= 999){
+        return in_words(Math.floor(num/100)) + ' Ratus ' + in_words(Math.floor(num/10));
     }
 }
 
 
-console.log(in_words(12));
+console.log(in_words(999));
